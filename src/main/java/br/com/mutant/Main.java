c51a16639c5fc3d8565c07c43e87a8e6a58ebb12
tiago <si.tiagoferreira@gmail.com>
2018-12-08 21:34:42 -0200

    verificando as linhas em sequencia

--- src/main/java/br/com/mutant/Main.java
+++ src/main/java/br/com/mutant/Main.java
@@ -6,44 +6,62 @@ public class Main {
 
 	public static void main(String[] args) {
 		Main main = new Main();
-		String[] dna = { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };
-//		String[] dna = {"ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTG"};
-//		boolean result = main.isMutant(dna);
-//		System.out.println(result);
-		boolean checkLine = main.checkLine("TTTAAT");
-		System.out.println(checkLine);
+//		String[] dna = { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };
+		String[] dna = {"ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTG"};
+		boolean result = main.isMutant(dna);
+		System.out.println(result);
 	}
-
-	private boolean checkLine(String dna) {
-		char letter = 'T';
-		int occurrence = countOccurrences(dna, letter);
-		int pos[] = new int[occurrence];
-		int size = dna.length();
-		
-		for(int i=0, j=0; i<size; i++) {			
-			if(dna.charAt(i) == letter) {
-				pos[j] = i;
-				j++;
-			}
-		}
-		
-		int count = 0;
-		for(int i=0; i < occurrence -1; i++) {
-			if( (pos[i] + 1) == pos[i+1]) {
-				count++;
-				if(count == 3) {
+	
+	public boolean isMutant(String[] dna) {
+		char[] possibleLetters = {'A','C','G', 'T' };
+		for(char letter : possibleLetters) {			
+			for(String line : dna) {
+				boolean value = checkLine(line, letter);
+				if(value) {
 					return true;
 				}
-			} else {
-				count = 0;
 			}
 		}
+		return false;
+	}
+
+	private boolean checkLine(String dna, char letter) {
+		int positionsOfOccurrenceCharacter[] = getPositionOcurrence(dna, letter);
+		int count = countOccurrenceInSequence(positionsOfOccurrenceCharacter);
 		if(count >= 3) {
 			return true;
 		}
 		return false;
 	}
 
+	private int[] getPositionOcurrence(String dna, char letter) {
+		int qtdOccurrence = countOccurrences(dna, letter);
+		int positionsOfOccurrenceCharacter[] = new int[qtdOccurrence];
+		for(int i=0, j=0; i<dna.length(); i++) {			
+			if(dna.charAt(i) == letter) {
+				positionsOfOccurrenceCharacter[j] = i;
+				j++;
+			}
+		}
+		return positionsOfOccurrenceCharacter;
+	}
+
+	private int countOccurrenceInSequence(int[] pos) {
+		int count = 0;
+		for(int i=0; i < pos.length -1; i++) {
+			if( (pos[i] + 1) == pos[i+1]) {
+				count++;
+				if(count == 3) { // case check occurrence in initial sequence return  
+					return 3;
+				}
+			} else {
+				count = 0;
+			}
+		}
+		return count;
+	}
+
+	//count occurrence characteres on string
 	private int countOccurrences(String someString, char someChar) {
 		int count =0;
 		for (int i = 0; i < someString.length(); i++) {
@@ -54,43 +72,5 @@ public class Main {
 		return count;
 	}
 
-//	public boolean isMutant(String[] dna) {
-//		boolean toReturn = false;
-//		int size = dna.length;
-//		char[][] aux = new char[size][size];
-//
-//		for (int i = 0; i < size; i++) {
-//			for (int j = 0; j < size; j++) {
-//				aux[i][j] = dna[i].charAt(j);
-//			}
-//		}
-//		char[] possibleLetters = { 'T' };
-//		toReturn = checkLines(aux, possibleLetters, size);
-//
-//		return toReturn;
-//	}
-//
-//	private boolean checkLines(char[][] values, char[] possibleLetters, int size) {
-//		int count = 0;
-//		for (char letter : possibleLetters) {
-//
-//			char old = letter;
-//			for (int line = 0; line < size; line++) {
-//				for (int column = 0; column < size; column++) {
-//					if (values[line][column] == letter && letter == old) {
-//						count++;
-//						System.out.println(letter + ":  pos[" + line + ", " + column + "]");
-//					}
-//					old = values[line][column];
-//
-//				}
-//				if (count >= 4) {
-//					return true;
-//				}
-//				count = 0;
-//			}
-//		}
-//		return false;
-//	}
 
 }
